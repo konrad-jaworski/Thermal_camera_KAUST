f0db0097a0f2a10feb9fa7069364f66f58a231b0
konrad-jaworski <konrad.agh.imir@gmail.com>
2025-01-17 09:24:21 +0300

    DMD implemented

--- ThermographyToolSet.py
+++ ThermographyToolSet.py
@@ -1,12 +1,13 @@
 import numpy as np
-import pandas as pd
 import glob
 import os
 import re
 from scipy.signal import savgol_filter
 from scipy.fft import fft
 from sklearn.decomposition import PCA
-
+from scipy import stats
+from scipy.linalg import svd, inv, eig
+from sklearn.preprocessing import MinMaxScaler
 
 def filtering(data,n_sample,poly_order):
     for i in range(data.shape[1]):
@@ -41,10 +42,11 @@ def ReadingDataToStack(csv_folder):
     return cropped_frames
 
 def phasegram(data):
+    phasegram_matrix=np.zeros(((data.shape[0],data.shape[1],data.shape[2])))
     for i in range(data.shape[1]):
         for j in range(data.shape[2]):
-            data[:,i,j] = np.angle(fft(data[:,i,j]))
-    return data
+            phasegram_matrix[:,i,j] = np.angle(fft(data[:,i,j]))
+    return phasegram_matrix
 
 def PCT(data,principal_component_number):
     reshaped_data=data.reshape(data.shape[0],data.shape[1]*data.shape[2])
@@ -55,3 +57,89 @@ def PCT(data,principal_component_number):
     PCT_data=np.transpose(PCT_data,(1,0))
     PCT_data=PCT_data.reshape(PCT_data.shape[0],data.shape[1],data.shape[2])
     return PCT_data
+
+def TSR(data,polynomial_order):
+    x=np.linspace(1,data.shape[0],data.shape[0])
+    x_log=np.log(x)
+
+    coefficient_matrix=np.zeros(((polynomial_order+1,data.shape[1],data.shape[2])))
+    for i in range(data.shape[1]):
+        for j in range(data.shape[2]):
+            coefficient_matrix[:,i,j]=np.polyfit(x_log,np.log(data[:,i,j]),polynomial_order)
+
+    return coefficient_matrix
+
+def TSR2RGB(data):
+    def normalize_channel(channel):
+        channel_min = np.min(channel)
+        channel_max = np.max(channel)
+        normalized = (channel - channel_min) / (channel_max - channel_min) * 255
+        return normalized.astype(np.uint8)
+
+    red_channel=normalize_channel(data[0])
+    green_channel=normalize_channel(data[1])
+    blue_channel=normalize_channel(data[2])
+
+    rgb_image = np.stack((red_channel, green_channel, blue_channel), axis=-1)
+    return rgb_image
+
+def HOS(data):
+    HOS_matrix=np.zeros(((3,data.shape[1],data.shape[2])))
+
+    skew_image=np.zeros((data.shape[1],data.shape[2]))
+    for i in range(data.shape[1]):
+        for j in range(data.shape[2]):
+            skew_image[i,j]=stats.skew(data[:,i,j])
+
+    kurtosis_image=np.zeros((data.shape[1],data.shape[2]))
+    for i in range(data.shape[1]):
+        for j in range(data.shape[2]):
+            kurtosis_image[i,j]=stats.kurtosis(data[:,i,j])
+
+    def fifth_standardized_central_moment(data):
+        mean = np.mean(data)
+        std_dev = np.std(data)
+        standardized_data = (data - mean) / std_dev
+        moment_5th = np.mean(standardized_data**5)
+        return moment_5th
+
+    fifth_moment=np.zeros((data.shape[1],data.shape[2]))
+
+    for i in range(data.shape[1]):
+        for j in range(data.shape[2]):
+            fifth_moment[i,j]=fifth_standardized_central_moment(data[:,i,j])
+
+
+    HOS_matrix[0,:,:]=skew_image
+    HOS_matrix[1,:,:]=kurtosis_image
+    HOS_matrix[2,:,:]=fifth_moment
+
+    return HOS_matrix
+
+def DMD(data,truncation):
+    re_thermo=data.reshape(data.shape[0],data.shape[1]*data.shape[2])
+    dmd_ready=np.transpose(re_thermo,(1,0))
+    scaler=MinMaxScaler()
+    dmd_ready=scaler.fit_transform(dmd_ready)
+
+    x=dmd_ready[:,:-1]
+    x_p=dmd_ready[:,1:]
+
+    U,s,Vh=svd(x,full_matrices=False)
+
+    sigma=np.zeros((Vh.shape[0],Vh.shape[1]))
+    for i in range(Vh.shape[0]):
+        sigma[i,i]=s[i]
+
+    U=U[:,:truncation]
+    Vh=Vh[:truncation,:]
+    sigma=sigma[:truncation,:truncation]
+
+    A_tylda=U.T@x_p@Vh.T@inv(sigma)
+    eig_val,eig_vec=eig(A_tylda)
+
+    omega=x_p@Vh.T@inv(sigma)@eig_vec
+    omega_tr=np.transpose(omega,(1,0))
+    dmd_data=omega_tr.reshape(omega_tr.shape[0],data.shape[1],data.shape[2])
+
+    return dmd_data
